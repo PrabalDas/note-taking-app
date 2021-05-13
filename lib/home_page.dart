f66fa6d5ad49f9576fa05773fe54d6d6e0204973
prabal_das <prabaldas94059@gmail.com>
2021-05-13 18:13:03 +0530

    day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Prabal";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+          child:
+              Container(child: Text('Welcome to $days of flutter by $name'))),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_taking_app/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,9 +8,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Prabal";
+
+    // bool ismale = true;
+    // num temp = 30.66;
+    // var day = "Tueday";
+    // const pi = 3.14;
+    // final
+
     return MaterialApp(
-      home: Material(
-          child: Center(child: Container(child: Text('Hey Im Prabal')))),
+      home: HomePage(),
     );
   }
 }
