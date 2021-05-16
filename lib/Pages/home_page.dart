8efb0839284fc77d9c60df09b37400c11379ccc7
prabal_das <prabaldas94059@gmail.com>
2021-05-16 17:44:16 +0530

    day 13 completed

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -44,14 +44,43 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-          ? ListView.builder(
+          ? GridView.builder(
+              physics: BouncingScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, mainAxisSpacing: 16, crossAxisSpacing: 16),
               itemBuilder: (context, index) {
-                return ItemWidget(
-                  item: CatalogModel.items[index],
-                );
+                final item = CatalogModel.items[index];
+                return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: GridTile(
+                        header: Container(
+                            padding: EdgeInsets.all(12),
+                            decoration: BoxDecoration(color: Colors.deepPurple),
+                            child: Text(
+                              item.name,
+                              style: TextStyle(color: Colors.white),
+                            )),
+                        footer: Container(
+                            padding: EdgeInsets.all(12),
+                            decoration: BoxDecoration(color: Colors.black),
+                            child: Text(
+                              item.price.toString(),
+                              style: TextStyle(color: Colors.white),
+                            )),
+                        child: Image.network(item.image)));
               },
               itemCount: CatalogModel.items.length,
             )
+          // ? ListView.builder(
+          //     itemBuilder: (context, index) {
+          //       return ItemWidget(
+          //         item: CatalogModel.items[index],
+          //       );
+          //     },
+          //     itemCount: CatalogModel.items.length,
+          //   )
           : Center(child: CircularProgressIndicator()),
       drawer: MyDrawer(),
     );
